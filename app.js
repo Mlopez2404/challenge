83ca84aea97dc646d079042e241ae45b602a041e
Maria Lopez <geral857@gmail.com>
2025-09-23 21:54:05 -0300

    se agrego el nombre al array y se limpia el input

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []
+
+function agregarAmigo() {
+    let inputNombre = document.getElementById('amigo');
+    let nombre = inputNombre.value;
+    if (nombre == '') {
+        alert('Por favor inserte un nombre');
+    }
+    else {
+        amigos.push(nombre);
+        inputNombre.value = '';
+        console.log(amigos);
+    }
+
+}
